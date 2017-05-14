f68021b1c82d0439697024995589603dc1892cb8
Greivin Britton <brittongr@gmail.com>
2017-05-14 16:59:22 -0600

    Fix import for Component class in Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from './box/Container';
 
-export class Demo extends Components
+export class Demo extends Component
 {
     render()
     {
